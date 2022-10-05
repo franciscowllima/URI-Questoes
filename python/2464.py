64feee8c5daead162fed3bd6d957e8a71041e7c1
lucas <lucascplusmart@gmail.com>
2022-10-05 19:35:43 -0300

    [beecrowd-2464] - Decifra

---
+++ python/2464.py
@@ -0,0 +1,14 @@
+chave = input()
+texto = input()
+palavra = []
+alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
+           "n", "o", "p", "q", "r", "s", "t", "u","v", "w", "x", "y", "z"]
+
+for i in range(len(texto)):
+    for j in range(len(chave)):
+        if texto[i] == chave[j]:
+            palavra.append(alfabeto[j])
+            
+print(''.join(palavra)) 
+
+
