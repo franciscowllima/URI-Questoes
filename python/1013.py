f5d7675a5a0fba43a5b56ea8606975eb3bc163d4
ilsonflim <114932972+ilsonflim@users.noreply.github.com>
2022-10-06 10:48:01 -0300

    Create 1013.py

---
+++ python/1013.py
@@ -0,0 +1,8 @@
+x = input().split(" ")
+
+a, b, c = x
+
+maior = (int(a) + int(b) + abs(int(a) - int(b)))  / 2
+resultado = (int(maior) + int(c) + abs(int(maior) - int(c)))/2
+
+print("%d eh o maior" %resultado)
