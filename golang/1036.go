dfcd40f036c90c39143d2f3f28cebea23ee5283e
Davi-Arauj <moreiradaavi@gmail.com>
2022-10-11 17:11:29 -0300

    [URI-1036] - Ajuste no calculo da função e na mensagem de erro, pois no site do beecrowd não estava com 100% de aceite

--- golang/1036.go
+++ golang/1036.go
@@ -8,8 +8,7 @@ import (
 
 func main() {
 	var (
-		a, b, c       float64
-		delta, r1, r2 float64
+		a, b, c, delta float64
 	)
 
 	_, err := fmt.Scanf("%f %f %f\n", &a, &b, &c)
@@ -18,20 +17,11 @@ func main() {
 		return
 	}
 
-	if a == 0.0 {
-		fmt.Println("Impossível de calcular")
+	if a == 0.0 || ((b*b)-4*a*c) < 0 {
+		fmt.Println("Impossivel calcular")
 		return
+	} else {
+		delta = math.Sqrt((b * b) - 4*a*c)
+		fmt.Printf("R1 = %.5f\nR2 = %.5f\n", ((-b + delta) / (2 * a)), ((-b - delta) / (2 * a)))
 	}
-
-	delta = math.Sqrt(math.Pow(b, 2.0) - 4*(a*c))
-
-	if math.IsNaN(delta) {
-		fmt.Println("Impossível de calcular")
-		return
-	}
-
-	r1 = ((-1 * b) + delta) / (2 * a)
-	r2 = ((-1 * b) - delta) / (2 * a)
-
-	fmt.Printf("%.5f %.5f\n", r1, r2)
 }
